030f3e292a6e71bb98a3ef0017bc4fd8f63081df
roroblack <58134693+roroblack@users.noreply.github.com>
2026-09-03 22:58:23 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer(num_list.begin() + n, num_list.end());
+    
+    answer.insert(answer.end(), num_list.begin(), num_list.begin() + n);
+    
+    return answer;
+}
